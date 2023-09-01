67d8429ae2c8aa36f457bd96b26c4c9ca59bfa62
Jesper van der Zwaag <63634085+jespervdz@users.noreply.github.com>
2023-09-01 21:09:50 +0200

    Update set-variables-and-build-windows.bat

--- build-scripts/set-variables-and-build-windows.bat
+++ build-scripts/set-variables-and-build-windows.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set BUILD_QMAKE=C:\Qt\Qt-5.14.2\5.14.2\mingw73_64\bin\qmake.exe
-set BUILD_MINGW_BIN=C:\Qt\Qt-5.14.2\Tools\mingw730_64\bin
-set BUILD_ARCHIVE_NAME=CasparCG Client
-set BUILD_PARALLEL_THREADS=8
+set BUILD_QMAKE=C:\Qt\Qt5.14.2\5.14.2\mingw73_64\bin\qmake.exe
+set BUILD_MINGW_BIN=C:\Qt\Qt5.14.2\Tools\mingw730_64\bin
+set BUILD_ARCHIVE_NAME=CasparCG Client RTV1
+set BUILD_PARALLEL_THREADS=16
 set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
 
 build-windows.bat
